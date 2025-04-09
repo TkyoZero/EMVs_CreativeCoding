9a66411659162db30fa3ee7d82b64fd9088e7e0b
T.T <tenzin.tob8@gmail.com>
2025-04-09 09:39:29 +0200

    create graphics sketch

---
+++ creative_coding/src/components/sketch/Graphics.vue
@@ -0,0 +1,62 @@
+<template>
+	<div ref="canvasContainer"></div>
+</template>
+
+<script>
+import p5 from "p5";
+
+export default {
+	name: "Graphics",
+	data() {
+		return {
+			p5Instance: null,
+		};
+	},
+	mounted() {
+		this.createCanvas();
+	},
+	beforeDestroy() {
+		if (this.p5Instance) {
+			this.p5Instance.remove(); // Proper cleanup
+		}
+	},
+	methods: {
+		createCanvas() {
+			this.p5Instance = new p5(this.sketch, this.$refs.canvasContainer); // Fixed variable name
+		},
+		sketch(p) {
+			let graphics;
+
+			p.setup = () => {
+				p.createCanvas(400, 400);
+				graphics = p.createGraphics(200, 200);
+				p.noStroke(); // Apply globally for cleaner shapes
+			};
+
+			p.draw = () => {
+				p.background(220);
+
+				// Main canvas drawing
+				p.fill(255, 0, 0);
+				p.rect(50, 50, 100, 100);
+
+				// Graphics buffer drawing
+				graphics.fill(0, 0, 255);
+				graphics.ellipse(
+					p.random(graphics.width),
+					p.random(graphics.height),
+					20,
+					20
+				);
+
+				// Display graphics buffer
+				p.image(graphics, 100, 100);
+			};
+
+			p.mousePressed = () => {
+				graphics.clear();
+			};
+		},
+	},
+};
+</script>
