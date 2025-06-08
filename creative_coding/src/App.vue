699321ea673782b2cd5ccf02f315034a1ef6addc
Tenzin Tobatsang <tenzin.tob8@gmail.com>
2025-06-09 00:34:06 +0200

    basic disfunktional dropdown

--- creative_coding/src/App.vue
+++ creative_coding/src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+
+const isOpen = ref(false);
+
+const toggleDropdown = () => {
+	isOpen.value = !isOpen.value;
+};
 </script>
 
 <template>
@@ -11,41 +18,89 @@ import Footer from "@/components/Footer.vue";
 			<div class="wrapper">
 				<nav>
 					<!-- ---------------------------- Sketches ---------------------------- -->
-					<RouterLink to="/sketches/loading">Loading</RouterLink>
-					<RouterLink to="/sketches/save-style">Save Style</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Sketches</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/sketches/loading">Loading</RouterLink></li>
+							<li><RouterLink to="/sketches/save-style">Save Style</RouterLink></li>
+						</ul>
+					</div>
 
 					<!-- ---------------------------- Task 01 ---------------------------- -->
-					<RouterLink to="/task/01/random-ball">Random Ball</RouterLink>
-					<RouterLink to="/task/01/random-squares">Random Squares</RouterLink>
-					<RouterLink to="/task/01/graphics">Graphic</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Task 01</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/task/01/random-ball">Random Ball</RouterLink></li>
+							<li><RouterLink to="/task/01/random-squares">Random Squares</RouterLink></li>
+							<li><RouterLink to="/task/01/graphics">Graphic</RouterLink></li>
+						</ul>
+					</div>
 
 					<!-- ---------------------------- Task 02 ---------------------------- -->
-					<RouterLink to="/task/02/image-manipulation">Image Manipulation</RouterLink>
-					<RouterLink to="/task/02/brightness-sort">Brightness Sort</RouterLink>
-					<RouterLink to="/task/02/dithering-effect">Dithering Effect</RouterLink>
-					<RouterLink to="/task/02/glitch-effect">Glitch Effect</RouterLink>
-					<RouterLink to="/task/02/image-resize">Image Resize</RouterLink>
-					<RouterLink to="/task/02/image-overlay">Image Overlay</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Task 02</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/task/02/image-manipulation">Image Manipulation</RouterLink></li>
+							<li><RouterLink to="/task/02/brightness-sort">Brightness Sort</RouterLink></li>
+							<li><RouterLink to="/task/02/dithering-effect">Dithering Effect</RouterLink></li>
+							<li><RouterLink to="/task/02/glitch-effect">Glitch Effect</RouterLink></li>
+							<li><RouterLink to="/task/02/image-resize">Image Resize</RouterLink></li>
+							<li><RouterLink to="/task/02/image-overlay">Image Overlay</RouterLink></li>
+						</ul>
+					</div>
 
 					<!-- ---------------------------- Task 03 ---------------------------- -->
-					<RouterLink to="/task/03/pipe-shape">Pipe Shape</RouterLink>
-					<RouterLink to="/task/03/circle-packing">Circle Packing</RouterLink>
-					<RouterLink to="/task/03/abstract-algorithm">Abstract Algoritm</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Task 03</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/task/03/pipe-shape">Pipe Shape</RouterLink></li>
+							<li><RouterLink to="/task/03/circle-packing">Circle Packing</RouterLink></li>
+							<li><RouterLink to="/task/03/abstract-algorithm">Abstract Algoritm</RouterLink></li>
+						</ul>
+					</div>
 
 					<!-- ---------------------------- Task 04 ---------------------------- -->
-					<RouterLink to="/task/04/crystal">Crystal</RouterLink>
-					<RouterLink to="/task/04/sierpinski-arrowhead">Sierpinski Arrowhead</RouterLink>
-					<RouterLink to="/task/04/fern">Fern</RouterLink>
-					<RouterLink to="/task/04/board">Board</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Task 04</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/task/04/crystal">Crystal</RouterLink></li>
+							<li><RouterLink to="/task/04/sierpinski-arrowhead">Sierpinski Arrowhead</RouterLink></li>
+							<li><RouterLink to="/task/04/fern">Fern</RouterLink></li>
+							<li><RouterLink to="/task/04/board">Board</RouterLink></li>
+						</ul>
+					</div>
 
 					<!-- ---------------------------- Task 05 ---------------------------- -->
-					<RouterLink to="/task/05/text-generator">Text Generator</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Task 05</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/task/05/text-generator">Text Generator</RouterLink></li>
+						</ul>
+					</div>
 
 					<!-- ---------------------------- Task 06 ---------------------------- -->
-					<RouterLink to="/task/06/original-ruleset">Game of Life</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Task 01</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/task/06/original-ruleset">Game of Life</RouterLink></li>
+						</ul>
+					</div>
 
 					<!-- ---------------------------- Task 07 ---------------------------- -->
-					<RouterLink to="/task/07/mandel-brot">Mandel Brot</RouterLink>
+					<div class="dropdown" @click.self="isOpen = false">
+						<button @click.stop="toggleDropdown">Project</button>
+
+						<ul v-if="isOpen" class="dropdown-menu">
+							<li><RouterLink to="/task/07/mandel-brot">Mandel Brot</RouterLink>s</li>
+						</ul>
+					</div>
 				</nav>
 			</div>
 		</header>
---
+++ creative_coding/src/components/Dropdown.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="dropdown" @click.self="onToggle(null)">
+		<button @click.stop="onToggle(title)">
+			{{ title }}
+		</button>
+
+		<ul v-if="isOpen" class="dropdown-menu">
+			<li v-for="(item, index) in items" :key="index">
+				<RouterLink :to="item.link">{{ item.label }}</RouterLink>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	title: String,
+	items: Array,
+	isOpen: Boolean,
+	onToggle: Function,
+});
+</script>
