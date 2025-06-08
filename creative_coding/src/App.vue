8f7441a5c3bb91a8a8c50c4b48aa6b6633a94037
Tenzin Tobatsang <tenzin.tob8@gmail.com>
2025-06-09 00:24:15 +0200

    style optimised

--- creative_coding/src/App.vue
+++ creative_coding/src/App.vue
@@ -1,52 +1,57 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-	<header>
-		<div class="wrapper">
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/sketches/loading">Loading</RouterLink>
-				<RouterLink to="/sketches/save-style">Save Style</RouterLink>
+	<main class="font-display bg-blue-200">
+		<Header />
+		<header>
+			<div class="wrapper">
+				<nav>
+					<!-- ---------------------------- Sketches ---------------------------- -->
+					<RouterLink to="/sketches/loading">Loading</RouterLink>
+					<RouterLink to="/sketches/save-style">Save Style</RouterLink>
 
-				<!-- ---------------------------- Task 01 ---------------------------- -->
-				<RouterLink to="/task/01/random-ball">Random Ball</RouterLink>
-				<RouterLink to="/task/01/random-squares">Random Squares</RouterLink>
-				<RouterLink to="/task/01/graphics">Graphic</RouterLink>
+					<!-- ---------------------------- Task 01 ---------------------------- -->
+					<RouterLink to="/task/01/random-ball">Random Ball</RouterLink>
+					<RouterLink to="/task/01/random-squares">Random Squares</RouterLink>
+					<RouterLink to="/task/01/graphics">Graphic</RouterLink>
 
-				<!-- ---------------------------- Task 02 ---------------------------- -->
-				<RouterLink to="/task/02/image-manipulation">Image Manipulation</RouterLink>
-				<RouterLink to="/task/02/brightness-sort">Brightness Sort</RouterLink>
-				<RouterLink to="/task/02/dithering-effect">Dithering Effect</RouterLink>
-				<RouterLink to="/task/02/glitch-effect">Glitch Effect</RouterLink>
-				<RouterLink to="/task/02/image-resize">Image Resize</RouterLink>
-				<RouterLink to="/task/02/image-overlay">Image Overlay</RouterLink>
+					<!-- ---------------------------- Task 02 ---------------------------- -->
+					<RouterLink to="/task/02/image-manipulation">Image Manipulation</RouterLink>
+					<RouterLink to="/task/02/brightness-sort">Brightness Sort</RouterLink>
+					<RouterLink to="/task/02/dithering-effect">Dithering Effect</RouterLink>
+					<RouterLink to="/task/02/glitch-effect">Glitch Effect</RouterLink>
+					<RouterLink to="/task/02/image-resize">Image Resize</RouterLink>
+					<RouterLink to="/task/02/image-overlay">Image Overlay</RouterLink>
 
-				<!-- ---------------------------- Task 03 ---------------------------- -->
-				<RouterLink to="/task/03/pipe-shape">Pipe Shape</RouterLink>
-				<RouterLink to="/task/03/circle-packing">Circle Packing</RouterLink>
-				<RouterLink to="/task/03/abstract-algorithm">Abstract Algoritm</RouterLink>
+					<!-- ---------------------------- Task 03 ---------------------------- -->
+					<RouterLink to="/task/03/pipe-shape">Pipe Shape</RouterLink>
+					<RouterLink to="/task/03/circle-packing">Circle Packing</RouterLink>
+					<RouterLink to="/task/03/abstract-algorithm">Abstract Algoritm</RouterLink>
 
-				<!-- ---------------------------- Task 04 ---------------------------- -->
-				<RouterLink to="/task/04/crystal">Crystal</RouterLink>
-				<RouterLink to="/task/04/sierpinski-arrowhead">Sierpinski Arrowhead</RouterLink>
-				<RouterLink to="/task/04/fern">Fern</RouterLink>
-				<RouterLink to="/task/04/board">Board</RouterLink>
+					<!-- ---------------------------- Task 04 ---------------------------- -->
+					<RouterLink to="/task/04/crystal">Crystal</RouterLink>
+					<RouterLink to="/task/04/sierpinski-arrowhead">Sierpinski Arrowhead</RouterLink>
+					<RouterLink to="/task/04/fern">Fern</RouterLink>
+					<RouterLink to="/task/04/board">Board</RouterLink>
 
-				<!-- ---------------------------- Task 05 ---------------------------- -->
-				<RouterLink to="/task/05/text-generator">Text Generator</RouterLink>
+					<!-- ---------------------------- Task 05 ---------------------------- -->
+					<RouterLink to="/task/05/text-generator">Text Generator</RouterLink>
 
-				<!-- ---------------------------- Task 06 ---------------------------- -->
-				<RouterLink to="/task/06/original-ruleset">Game of Life</RouterLink>
+					<!-- ---------------------------- Task 06 ---------------------------- -->
+					<RouterLink to="/task/06/original-ruleset">Game of Life</RouterLink>
 
-				<!-- ---------------------------- Task 07 ---------------------------- -->
-				<RouterLink to="/task/07/mandel-brot">Mandel Brot</RouterLink>
-			</nav>
-		</div>
-	</header>
-
-	<RouterView />
+					<!-- ---------------------------- Task 07 ---------------------------- -->
+					<RouterLink to="/task/07/mandel-brot">Mandel Brot</RouterLink>
+				</nav>
+			</div>
+		</header>
+		<RouterView />
+		<Footer />
+	</main>
 </template>
 
 <style scoped></style>
---
+++ creative_coding/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+	<footer class="p-20 bg-teal-800 text-white">
+		<div class="text-center">&copy Tenzin Tobatsang</div>
+	</footer>
+</template>
